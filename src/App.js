c3091fc52fa091662020ddb83228ed62a7ebc19b
Junho Moon <112556572+ProdMoon@users.noreply.github.com>
2023-04-09 20:53:49 +0900

    feat: minimum service developed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [text, setText] = useState(0);
+
+  useEffect(() => {
+    const autosaved = localStorage.getItem('autosaved');
+    if (autosaved) {
+      const textarea = document.getElementById("text");
+      textarea.value = autosaved;
+      setText(textarea.value.length);
+    }
+  }, [])
+
+  function handleTextareaChange() {
+    const textarea = document.getElementById("text");
+    setText(textarea.value.length);
+    localStorage.setItem("autosaved", textarea.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea
+        className="border rounded-md p-5 m-5"
+        id="text"
+        cols="120"
+        rows="40"
+        onChange={handleTextareaChange}
+      ></textarea>
+      <h1>{text}</h1>
     </div>
   );
 }
